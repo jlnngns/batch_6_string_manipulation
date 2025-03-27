4cacb7e8d6947e9636bada35f30ce98971e1183a
Julianna Gines <jmgines.12@gmail.com>
2025-03-27 21:07:58 +0800

    Update remove_prefix_without_using_removeprefix().py

--- remove_prefix_without_using_removeprefix().py
+++ remove_prefix_without_using_removeprefix().py
@@ -3,3 +3,6 @@ def custom_removeprefix(me, prefix):
          return me[len(prefix):]
     return me
 
+text = input("Enter a string: ")
+prefix = input("Enter prefix to remove: ")
+print("", custom_removeprefix(text, prefix))
