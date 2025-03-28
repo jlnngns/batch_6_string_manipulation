48f075b58f22374aef19c0bc1c8f64f9bb735e9f
Julianna Gines <jmgines.12@gmail.com>
2025-03-29 07:52:40 +0800

    Create b7.1_remove_trailing_spaces without_rsplit().py

---
+++ b7.1_remove_trailing_spaces without_rsplit().py
@@ -0,0 +1,8 @@
+def custom_rsplit(text):
+    index = len(text) - 1
+    while index >= 0 and text[index] == ' ':
+        index -= 1
+    return text[:index+1]
+
+text == input("Enter a string with trailing spaces: ")
+print("'" + custom_rsplit(text) + "'")
