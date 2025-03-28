45393f726ad18b9f10d6092aa4e6b992d006c149
Julianna Gines <jmgines.12@gmail.com>
2025-03-28 10:40:06 +0800

    Update uppercase_characters_without_using_isupper().py

--- uppercase_characters_without_using_isupper().py
+++ uppercase_characters_without_using_isupper().py
@@ -1,8 +1,5 @@
-def custom_isupper(sea):
-    for char in sea:
-        if 'a' <= 'z':
-            return False
-    return True
+def custom_isupper(input_string):
+    return any('A' <= c <= 'Z' for c in input_string) and not any('a' <= c <= 'z' for c in input_string)
 
-text = input("Enter a string: ")
-print("", custom_isupper(text))
+user_input = input("Enter a string: ")
+print("Is the string uppercase?", custom_isupper(user_input))
