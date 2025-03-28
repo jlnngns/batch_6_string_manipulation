0b21fa9b7174400c0cadb28385f52ba54f7dbfa5
Julianna Gines <jmgines.12@gmail.com>
2025-03-28 10:46:36 +0800

    Update remove_prefix_without_using_removeprefix().py

--- remove_prefix_without_using_removeprefix().py
+++ remove_prefix_without_using_removeprefix().py
@@ -1,8 +1,8 @@
-def custom_removeprefix(me, prefix):
-    if me.startswith(prefix):
-         return me[len(prefix):]
-    return me
+def custom_removeprefix(text, prefix):
+    if text.startswith(prefix):
+         return text[len(prefix):]
+    return text
 
-text = input("Enter a string: ")
+user_input = input("Enter a string: ")
 prefix = input("Enter prefix to remove: ")
-print("", custom_removeprefix(text, prefix))
+print("", custom_removeprefix(user_input, prefix))
