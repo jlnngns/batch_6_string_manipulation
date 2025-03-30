90ea9c9704145e21e008c7913cb2b97d9d724de1
Julianna Gines <jmgines.12@gmail.com>
2025-03-30 14:55:01 +0800

    Create b7.7_zero_fill_without_zfill().py

---
+++ b7.7_zero_fill_without_zfill().py
@@ -0,0 +1,6 @@
+def custom_zfill(text, width):
+    return '0' * (width - len(text)) + text if len(text) < width else text
+
+text = input("Enter a string: ")
+width = int(input("Enter total width: ")
+print("'" + custom_zfill(text, width) + "'")
