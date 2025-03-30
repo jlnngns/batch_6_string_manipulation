3dfa9ae6b27fac4829d9a7403f4adc0504e99471
Julianna Gines <jmgines.12@gmail.com>
2025-03-30 10:30:28 +0800

    Update b7.3_convert_to_uppercase_without_upper().py

--- b7.3_convert_to_uppercase_without_upper().py
+++ b7.3_convert_to_uppercase_without_upper().py
@@ -1,5 +1,5 @@
 def custom_upper(text):
-    return ''.join(chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in text)
+    return ''.join(chr(ord(cue) - 32) if 'a' <= cue <= 'z' else cue for cue in text)
 
 text = input("Enter a string: ")
 print("", custom_upper(text))
